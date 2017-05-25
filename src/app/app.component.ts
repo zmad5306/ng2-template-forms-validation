e881558bfe9b3d05ec5028726968b1f6793fe990
Zach Maddox <zachary.maddox@gmail.com>
2017-05-25 18:35:25 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+export class Account {
+  constructor(public password: string, public confirmPassword: string) {}
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  account: Account = new Account('', '');
+  submit(): void {
+    console.log(this.account);
+  }
 }
---
+++ src/app/must-match.directive.ts
@@ -0,0 +1,36 @@
+import { Directive, forwardRef, Attribute } from '@angular/core';
+import { Validator, AbstractControl, NG_VALIDATORS, FormGroup } from '@angular/forms';
+
+@Directive({
+  selector: '[appMustMatch][ngModelGroup]',
+  providers: [
+        { provide: NG_VALIDATORS, useExisting: forwardRef(() => MustMatchDirective), multi: true }
+    ]
+})
+export class MustMatchDirective implements Validator {
+
+  constructor() { }
+
+  validate(c: FormGroup) {
+    let valid = true;
+    //this fires before the form is done and throws an error becuase the input files aren't built
+    if (c.contains('password') && c.contains('confirmPassword')) {
+      if (c.controls.password.value !== c.controls.confirmPassword.value) {
+        valid = false;
+        c.controls.confirmPassword.setErrors({notMatched: true});
+      }
+      else {
+        c.controls.confirmPassword.setErrors(null);
+      }
+    }
+
+    if (!valid) {
+      return {notMatched: true};
+    }
+    else {
+      return null;
+    }
+    
+  }
+
+}
